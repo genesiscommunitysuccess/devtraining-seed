1f55e18b991a8a3ee71e55f967805e2aeacef7bf
dbarros1979 <daniel.barros@genesis.global>
2022-10-18 23:30:22 +0000

    Using GSF 6.1.7

--- server/jvm/settings.gradle.kts
+++ server/jvm/settings.gradle.kts
@@ -11,7 +11,7 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
         maven {
-            url = uri("https://genesisglobal.jfrog.io/genesisglobal/libs-release-client")
+            url = uri("https://genesisglobal.jfrog.io/genesisglobal/dev-repo")
             credentials {
                 username = extra.properties["genesisArtifactoryUser"].toString()
                 password = extra.properties["genesisArtifactoryPassword"].toString()
