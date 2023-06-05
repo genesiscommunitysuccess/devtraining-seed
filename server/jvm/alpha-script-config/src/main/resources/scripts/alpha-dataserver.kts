f445c536e1d46a67c9d567b6bf0004833434098d
Guilherme Guerra <guilherme.guerra@genesis.global>
2023-06-05 19:55:20 +0000

    add queries
    Signed-off-by: Guilherme Guerra <guilherme.guerra@genesis.global>

--- server/jvm/alpha-script-config/src/main/resources/scripts/alpha-dataserver.kts
+++ server/jvm/alpha-script-config/src/main/resources/scripts/alpha-dataserver.kts
@@ -1,9 +1,22 @@
+import javax.xml.bind.PrintConversionEvent
+
+/**
+ * System              : Genesis Business Library
+ * Sub-System          : multi-pro-code-test Configuration
+ * Version             : 1.0
+ * Copyright           : (c) Genesis
+ * Date                : 2022-03-18
+ * Function : Provide dataserver config for multi-pro-code-test.
+ *
+ * Modification History
+ */
+
 dataServer {
-    query("ALL_TRADES", TRADE_VIEW)
+    query("ALL_TRADES", TRADE)
     query("ALL_PRICES", TRADE){
         fields{
-            TRADE.PRICE
-            TRADE.SYMBOL
+            PRICE
+            SYMBOL
         }
         where { trade -> trade.price > 0.0 }
     }
