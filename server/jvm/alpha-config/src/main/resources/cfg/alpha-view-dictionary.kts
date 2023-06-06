2e239fd3bde62e62f06c27fc29415c99a8e53f8d
GuilhermeGuerraMarti <guilherme.guerra@genesis.global>
2023-06-06 18:35:10 +0000

    Exercise 3.2

--- server/jvm/alpha-config/src/main/resources/cfg/alpha-view-dictionary.kts
+++ server/jvm/alpha-config/src/main/resources/cfg/alpha-view-dictionary.kts
@@ -13,22 +13,36 @@ views {
     view("TRADE_VIEW", TRADE) {
 
         joins {
+            joining(COUNTERPARTY) {
+                on(TRADE.COUNTERPARTY_ID to COUNTERPARTY { COUNTERPARTY_ID })
+            }
             joining(INSTRUMENT) {
                 on(TRADE.INSTRUMENT_ID to INSTRUMENT { INSTRUMENT_ID })
             }
-            joining(COUNTERPARTY){
-                on(TRADE.COUNTERPARTY_ID to COUNTERPARTY { COUNTERPARTY_ID })
-            }
         }
-
 
         fields {
             TRADE.allFields()
 
+            COUNTERPARTY.COUNTERPARTY_NAME
             INSTRUMENT.INSTRUMENT_NAME
             INSTRUMENT.MARKET_ID withPrefix INSTRUMENT
             INSTRUMENT.CURRENCY_ID withAlias "CURRENCY"
-            COUNTERPARTY.COUNTERPARTY_NAME
+
+            derivedField("CONSIDERATION", DOUBLE) {
+                withInput(TRADE.QUANTITY, TRADE.PRICE) { QUANTITY, PRICE ->
+                    QUANTITY * PRICE
+                }
+            }
+
+            derivedField("TRADE_ASSET_CLASS", STRING) {
+                withInput(INSTRUMENT.ASSET_CLASS) {ASSET_CLASS ->
+                    if (ASSET_CLASS == null) {
+                        "UNKOWN"
+                    }
+                    ASSET_CLASS
+                }
+            }
         }
     }
 }
